a42084b357538e64dc9426b0f2708ab6e58b6cd0
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-08-02 13:27:06 +0300

    functions to save new credntials and find them

--- run.py
+++ run.py
@@ -10,3 +10,12 @@ def create_new_credential(account_name, account_password):
     """Function to create a new account and its credentials"""
     new_credential = Credentials(account_name, account_password)
     return new_credential
+
+def save_new_credential(credentials):
+    """Function to save the newly created account and password"""
+    credentials.save_credentials()
+
+
+def find_credential(account_name):
+    """Function that finds credentials based on account_name given"""
+    return Credentials.find_by_name(account_name)
