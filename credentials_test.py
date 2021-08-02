ce6fd041382e3352ae42d4475e3aca79d3a1cbd8
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-08-02 13:12:44 +0300

    setting up a setUp method

--- credentials_test.py
+++ credentials_test.py
@@ -5,3 +5,9 @@ class TestCredentials(unittest.TestCase):
     """
     Test class that defines test cases for the Credentials class behavior
     """
+    def setUp(self):
+            """
+            Set up method to run  before each test case
+            
+            """
+    self.new_credentials = Credentials("Facebook", "12345")
