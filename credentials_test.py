91d97aec789c0546179addfe8e5d63e57a502104
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-08-02 13:11:07 +0300

    class that defines test cases for the Credentials

--- credentials_test.py
+++ credentials_test.py
@@ -1,2 +1,7 @@
 import unittest
 from credentials import Credentials
+
+class TestCredentials(unittest.TestCase):
+    """
+    Test class that defines test cases for the Credentials class behavior
+    """
