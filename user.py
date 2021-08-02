5d8a42885798f7bd355b0ec9a706f9c4a68f7863
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-08-02 12:22:44 +0300

    initialize the username and password

--- user.py
+++ user.py
@@ -3,3 +3,8 @@ class User:
     Class that generates new instances of User
     
     """
+    
+def __init__(self, user_name, password):
+    self.user_name = user_name
+    self.password = password
+    user_list = []
