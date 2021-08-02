9f73054a840299df7734a72ec8846ea14f6a3be8
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-08-02 13:25:31 +0300

    define a function to create new accounts

--- run.py
+++ run.py
@@ -6,3 +6,7 @@ from credentials import Credentials
 import string
 
 # Functions to add credentials
+def create_new_credential(account_name, account_password):
+    """Function to create a new account and its credentials"""
+    new_credential = Credentials(account_name, account_password)
+    return new_credential
