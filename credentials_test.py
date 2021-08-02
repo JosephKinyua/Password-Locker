5466001ead1932db34b8a567792cee343923884f
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-08-02 13:13:54 +0300

    check wether the the new cdedentials have been initialized correctly

--- credentials_test.py
+++ credentials_test.py
@@ -11,3 +11,8 @@ class TestCredentials(unittest.TestCase):
             
             """
     self.new_credentials = Credentials("Facebook", "12345")
+    
+     def test_credentials_instance(self):
+            """Method that tests whether the new_credentials have been instantiated correctly"""
+        self.assertEqual(self.new_credentials.account_name, "Facebook")
+        self.assertEqual(self.new_credentials.account_password, "12345")
