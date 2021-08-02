0766c9d536573bf0bae7537e89cec86adb446673
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-08-02 12:36:37 +0300

    Set Up Method to run before each test case

--- test_user.py
+++ test_user.py
@@ -7,3 +7,11 @@ class TestUser(unittest.TestCase):
     Test class that defines test cases for the contact class behaviours.
     
     """
+    
+    def setUp(self):
+        """
+        
+        Set Up Method to run before each test case to check if the class has been instantiated correctly
+        
+        """
+        self.new_user = User("NewUser", "12345")
