04b5e5453dc82ce10b832ad7f399e2af57951f13
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-08-02 12:25:23 +0300

    method that saves user objects into the user list

--- user.py
+++ user.py
@@ -8,3 +8,11 @@ def __init__(self, user_name, password):
     self.user_name = user_name
     self.password = password
     user_list = []
+    
+def save_user(self):
+    """
+    
+    Method that saves user objects into user_list
+    
+    """
+    self.user_list.append(self)
