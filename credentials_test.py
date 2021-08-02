e80bd59a435d32fd01325dc565733ca1651a6bea
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-08-02 13:14:50 +0300

    check wether  the credential created have been saved

--- credentials_test.py
+++ credentials_test.py
@@ -16,3 +16,8 @@ class TestCredentials(unittest.TestCase):
             """Method that tests whether the new_credentials have been instantiated correctly"""
         self.assertEqual(self.new_credentials.account_name, "Facebook")
         self.assertEqual(self.new_credentials.account_password, "12345")
+        
+          def test_save_credentials(self):
+            """Method that tests whether the new credential created has been saved"""
+        self.new_credentials.save_credentials()
+        self.assertEqual(len(Credentials.credentials_list), 1)
