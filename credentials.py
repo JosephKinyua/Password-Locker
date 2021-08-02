d912a35e86c94974e3850cde5d0f436ee998815f
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-08-02 13:10:25 +0300

    import a unittest and the credential class

--- credentials.py
+++ credentials.py
@@ -1,30 +1,22 @@
 class Credentials:
-       """
-       Create class for credentials
-       """
-def __init__(self, account_name, account_password):
-    self.account_name = account_name
-    self.account_password = account_password
-    
-credentials_list = []
+    """Create class for credentials"""
 
-def save_credentials(self):
-        """
-        Method that saves credential objects into credentials_list
-        
-        """
+    def __init__(self, account_name, account_password):
+        self.account_name = account_name
+        self.account_password = account_password
+
+    credentials_list = []
+
+    def save_credentials(self):
+        """Method that saves credential objects into credentials_list"""
         self.credentials_list.append(self)
-        
-        def delete_credential(self):
-            """
-            Method which deletes a particular credential
-            
-            """
+
+    def delete_credential(self):
+        """Method which deletes a particular credential"""
         Credentials.credentials_list.remove(self)
-        
-        
-@classmethod
-def find_by_name(cls, account_name):
+
+    @classmethod
+    def find_by_name(cls, account_name):
         """Method that takes in a name and returns a credential that matches that particular name
         Args:
             name: account_name that has a password
@@ -35,8 +27,8 @@ def find_by_name(cls, account_name):
         for credential in cls.credentials_list:
             if credential.account_name == account_name:
                 return credential
-            
-            @classmethod
+
+    @classmethod
     def credential_exists(cls, name):
         """Method to check whether a credential exists
         Args:
@@ -49,7 +41,7 @@ def find_by_name(cls, account_name):
                 return True
         return False
 
- @classmethod
+    @classmethod
     def display_credentials(cls):
         """Method which displays all current credentials"""
         return cls.credentials_list
