4b864160160bc364f8e769e538d842d76e9f7f3a
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-08-02 12:57:16 +0300

    modified user.py and user_test.py

--- user.py
+++ user.py
@@ -1,18 +1,16 @@
 class User:
+    """Create class for users
     """
-    Class that generates new instances of User
-    
-    """
-    
-def __init__(self, user_name, password):
-    self.user_name = user_name
-    self.password = password
+
+    def __init__(self, user_name, password):
+        self.user_name = user_name
+        self.password = password
     user_list = []
-    
-def save_user(self):
-    """
-    Method that saves user objects into user_list
-    """
-    self.user_list.append(self)
-if __name__ ==  '__main__':
+
+    def save_user(self):
+        """Method that saves user objects into user_list"""
+        self.user_list.append(self)
+
+
+if __name__ == '__main__':
     main()
