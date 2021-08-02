23c8875dfce109fb02255306196b527356bbf507
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-08-02 13:15:34 +0300

    method that saves multiplemcredentils

--- credentials_test.py
+++ credentials_test.py
@@ -21,3 +21,10 @@ class TestCredentials(unittest.TestCase):
             """Method that tests whether the new credential created has been saved"""
         self.new_credentials.save_credentials()
         self.assertEqual(len(Credentials.credentials_list), 1)
+        
+         def test_save_multiple_credentials(self):
+            """Method that saves multiple credentials to credentials_list"""
+        self.new_credentials.save_credentials()
+        new_test_credential = Credentials("Twitter", "56789")
+        new_test_credential.save_credentials()
+        self.assertEqual(len(Credentials.credentials_list), 2)
